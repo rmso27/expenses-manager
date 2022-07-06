a60ae3dd3608c58c6c228da960cd4862ab1d4385
rmso27 <rubenmsoliveira@gmail.com>
2022-07-06 18:01:18 +0100

    created test route

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,12 +1,23 @@
 <script>
 
+    // Function to test connectivity with the BE
     async function testRoute() {
         const response = await fetch('http://127.0.0.1:5000/test')
         const result = await response.json()
 
-        console.log(result)
+        return result
     }
 
-    testRoute()
+    // Function to display test result in the FE
+    function displayResult(result) {
+        document.getElementById("result").innerHTML = result['response']
+    }
+
+    // Call testRoute function
+    testRoute().then(result => {
+        displayResult(result)
+    })
 
 </script>
+
+<p id = "result"></p>
