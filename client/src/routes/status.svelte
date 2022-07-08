5695efd734d037205b25e024d30877c7e7a79210
rmso27 <rubenmsoliveira@gmail.com>
2022-07-08 16:04:19 +0100

    updated FE display

--- client/src/routes/status.svelte
+++ client/src/routes/status.svelte
@@ -1,9 +1,14 @@
 <script>
 
     // Check the status of connectivity with the BE
-    fetch('http://127.0.0.1:5000/status').then(response => {
+    fetch('http://127.0.0.1:5000/status').then((response) => {
         return response.json()
-    }).then(result => {displayResult(result)})
+    }).then((result) => {
+        displayResult(result)
+    }).catch(() => { // f no communication with the BE
+        const result = {'server': 'FAILED', 'database': 'FAILED'}
+        displayResult(result)
+    })
 
     // Function to display the status results in the FE
     function displayResult(result) {
@@ -13,5 +18,18 @@
 
 </script>
 
-Server: <span id = "server"></span> <br />
-Database: <span id = "database"></span>
+<div class = "status-table">
+    <div class = "status-table-wrapper">
+        <div class = "core-status">
+            <div class = "core-status-header">
+                <h2>Core</h2>
+            </div>
+            <div class = "core-status-item">
+                Server: <span id = "server"></span>
+            </div>
+            <div class = "core-status-item">
+                Database: <span id = "database"></span>
+            </div>
+        </div>
+    </div>
+</div>
