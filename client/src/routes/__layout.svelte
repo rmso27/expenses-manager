6cb4b195fa8d3de5789b214560f733372255045a
rmso27 <rubenmsoliveira@gmail.com>
2022-07-08 16:03:42 +0100

    refactor

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -4,10 +4,6 @@
 </script>
 
 <div class = "main">
-    <div class = "main-wrapper">
-        <slot></slot>
-    </div>
-    <div class = "footer">
-        <Navbar/>
-    </div>
+    <Navbar/>
+    <slot></slot>
 </div>
