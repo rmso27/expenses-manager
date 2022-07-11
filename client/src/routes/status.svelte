21bc513324cd401a1fb90b92f6f8043bc51bca07
rmso27 <rubenmsoliveira@gmail.com>
2022-07-11 22:01:31 +0100

    removed HTML and added console.table display

--- client/src/routes/status.svelte
+++ client/src/routes/status.svelte
@@ -4,32 +4,10 @@
     fetch('http://127.0.0.1:5000/status').then((response) => {
         return response.json()
     }).then((result) => {
-        displayResult(result)
-    }).catch(() => { // f no communication with the BE
+        console.table([result])
+    }).catch(() => { // if no communication with the BE
         const result = {'server': 'FAILED', 'database': 'FAILED'}
-        displayResult(result)
+        console.table([result])
     })
 
-    // Function to display the status results in the FE
-    function displayResult(result) {
-        document.getElementById("server").innerHTML = result['server']
-        document.getElementById("database").innerHTML = result['database']
-    }
-
 </script>
-
-<div class = "status-table">
-    <div class = "status-table-wrapper">
-        <div class = "core-status">
-            <div class = "core-status-header">
-                <h2>Core</h2>
-            </div>
-            <div class = "core-status-item">
-                Server: <span id = "server"></span>
-            </div>
-            <div class = "core-status-item">
-                Database: <span id = "database"></span>
-            </div>
-        </div>
-    </div>
-</div>
