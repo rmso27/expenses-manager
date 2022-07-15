1ced5549232a92c3abae8da7c26a7b0177a9b73e
rmso27 <rubenmsoliveira@gmail.com>
2022-07-15 16:24:08 +0100

    main layout customization

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,9 +1,13 @@
 <script>
     import Navbar from "../components/Navbar.svelte"
-    import "../public/css/style.css"
+    import "../public/css/main.css"
+    import "../public/css/Navbar.css"
+    import "../public/css/Login.css"
 </script>
 
 <div class = "main">
     <Navbar/>
-    <slot></slot>
+    <div class="main-container">
+        <slot></slot>
+    </div>
 </div>
