7ed4daf928eb505208fbb7abc4553ad7401686a6
rmso27 <rubenmsoliveira@gmail.com>
2022-07-15 16:23:50 +0100

    login screen customization

---
+++ client/src/routes/login.svelte
@@ -0,0 +1,28 @@
+<script>
+    let src = "images/blue-circle.png"
+</script>
+
+<div class = "login-container">
+    <div class = "login-logo-container">
+        <!-- <img src = {src} alt = "image"> -->
+    </div>
+    <h1 class = "header">Welcome!</h1>
+    <form class = "login-form" action = "">
+        <div class="login-form-field">
+            <label>USERNAME:</label>
+            <input type = "email">
+        </div>
+        <div class = "login-form-field">
+            <label>PASSWORD:</label>
+            <input type = "password">
+        </div>
+        <div class = "login-form-options">
+            <a href = "#">Forgot your password?</a>
+        </div>
+        <button>LOGIN</button>
+        <div class = "login-form-options">
+            New to app <a href = "#"> Create account</a>
+        </div>
+    </form>
+
+</div>
